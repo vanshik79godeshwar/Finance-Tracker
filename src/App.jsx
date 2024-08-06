d1a0ea62f99abebd202272ddfc121c54a3ad4fb6
sanjanashah05 <sanjanashah696969@gmail.com>
2024-08-06 18:29:43 +0530

    this is the working code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Page/Home.jsx';
@@ -6,29 +7,31 @@ import Login from './Page/Login.jsx';
 import User from './Page/User.jsx';
 import PrivateRoute from './PrivateRoute.jsx';
 import Test from './Page/Test.jsx';
-import Career from './Page/Career.jsx'
+import Career from './Page/Career.jsx';
 import ContactUs from './Page/ContactUs.jsx';
-//import CareerOpenings from './Page/Career.jsx';
- import AboutUs from './Page/AboutUs.jsx';
+import AboutUs from './Page/AboutUs.jsx';
 import Services from './Page/Services.jsx';
+ 
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Blogs" element={<Test />} />
-        <Route path="/Career" element={<Career />} />
-       <Route path="/ContactUs" element={<ContactUs />} />
-       <Route path ="/AboutUs" element ={<AboutUs />} />
-        <Route path ="/Services" element={<Services />} />
-        <Route
-          path="/:username/*"
-          element={<PrivateRoute element={<User />} />}
-        />
-      </Routes>
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Blogs" element={<Test />} />
+          <Route path="/Career" element={<Career />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/Services" element={<Services />} />
+          <Route
+            path="/:username/*"
+            element={<PrivateRoute element={<User />} />}
+          />
+        </Routes>
+     
     </Router>
   );
 }
